bd6e4ab08ed27b9984aa4c032635e67424a97f91
odonskova <ohorbal10@gmail.com>
2020-11-10 15:07:44 +0200

    change webpack file

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,9 @@ module.exports = {
             react: path.join(__dirname, 'node_modules', 'react'),
         },
     },
+    performance: {
+        hints: false
+    },
     module: {
         rules: [
             {
